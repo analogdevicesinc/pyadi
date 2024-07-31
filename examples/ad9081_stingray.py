89966e51921b6b09aa2aa4dbf4323c9f5d606ca5
PopPaul2021 <Paul.Pop@analog.com>
2024-07-31 15:50:08 +0300

    checkpoint
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- examples/ad9081_stingray.py
+++ examples/ad9081_stingray.py
@@ -9,29 +9,33 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-url = "ip:analog-2.local" if len(sys.argv) == 1 else sys.argv[1]
-ssh = adi.sshfs(address=url, username="root", password="analog")
+url = "local:"
+ssh_url = "local:" if len(sys.argv) == 1 else sys.argv[1]
+ssh = adi.sshfs(address=ssh_url, username="root", password="analog")
 
-tx_offload_base_addr = 0x9c440000
-rx_offload_base_addr = 0x9c450000
-tx_dma_base_addr     = 0x9c430000
-rx_dma_base_addr     = 0x9c420000
-reset_reg_offload    = 0x84
-control_reg_offload  = 0x88
-control_reg_dma      = 0x400
-flags_reg_dma        = 0x40c
+tx_offload_base_addr    = 0x9c440000
+rx_offload_base_addr    = 0x9c450000
+tx_dma_base_addr        = 0x9c430000
+rx_dma_base_addr        = 0x9c420000
+tddn_base_adress        = 0x9c460000
+reset_reg_offload       = 0x84
+control_reg_offload     = 0x88
+x_length_dma            = 0x418
+control_reg_dma         = 0x400
+flags_reg_dma           = 0x40c
+offload_transfer_length = 0x1c
 
 # Reset the DMAs
 ssh._run(f"busybox devmem 0x{tx_dma_base_addr + control_reg_dma:02x} 32 0x0")
-# ssh._run(f"busybox devmem 0x{rx_dma_base_addr + control_reg_dma:02x} 32 0x0")
-# 
+ssh._run(f"busybox devmem 0x{rx_dma_base_addr + control_reg_dma:02x} 32 0x0")
+
 # Reset the TX offload
 ssh._run(f"busybox devmem 0x{tx_offload_base_addr + reset_reg_offload:02x} 32 0x0")
 ssh._run(f"busybox devmem 0x{tx_offload_base_addr + reset_reg_offload:02x} 32 0x1")
 
 # Reset the RX offload
-# ssh._run(f"busybox devmem 0x{rx_offload_base_addr + reset_reg_offload:02x} 32 0x0")
-# ssh._run(f"busybox devmem 0x{rx_offload_base_addr + reset_reg_offload:02x} 32 0x1")
+ssh._run(f"busybox devmem 0x{rx_offload_base_addr + reset_reg_offload:02x} 32 0x0")
+ssh._run(f"busybox devmem 0x{rx_offload_base_addr + reset_reg_offload:02x} 32 0x1")
 
 conv = adi.ad9081(url)
 tddn = adi.tddn(url)
@@ -64,15 +68,14 @@ conv.rx_ddr_offload = False
 stdout, stderr = ssh._run(f"busybox devmem 0x{tx_offload_base_addr + control_reg_offload:02x} 32 0x2")
 stdout, stderr = ssh._run(f"busybox devmem 0x{rx_offload_base_addr + control_reg_offload:02x} 32 0x2")
 
-
-samples_per_frame_desired = 2**15
-frame_length_ms = 20
-pulse_width_high = samples_per_frame_desired / conv.tx_sample_rate * 100
+samples_per_frame_desired = 245760
+frame_length_ms = 1
+pulse_width_high = (samples_per_frame_desired / conv.tx_sample_rate) * 100
 
 tddn.enable = 0
 tddn.startup_delay_ms = 0
 tddn.frame_length_ms = frame_length_ms
-tddn.burst_count = 0 
+tddn.burst_count = 0
 
 # TDD ENABLE
 
@@ -100,6 +103,13 @@ for chan in [3,4,5] :
     tddn.channel[chan].enable = 1
  
 tddn.enable = 1
+
+# with open("tdd_regdump.txt", "w") as f:
+#     for i in range(0,0x0b0,4):
+#            stdout, stderr = ssh._run(f"busybox devmem 0x{tddn_base_adress + i:02x}")
+#            print(f"0x{tddn_base_adress + i:02x} (Relative {i:02x}): {stdout}")
+#            f.write(f"0x{tddn_base_adress + i:02x}, (Relative {i:02x}), {stdout}\n")
+
 # Create and send data to TX DMA
 
 fs = int(conv.tx_sample_rate)
@@ -124,39 +134,50 @@ print(f"RX_recieve time[ms]: {((1/conv.rx_sample_rate) * N_rx)*1000}")
 print(f"TDD_frame time[ms]: {frame_length_ms}")
 
 A = 0.9 * 2**15  # -6 dBFS
-data_gap = 1500
+data_gap = int((0.05*samples_per_frame_desired)/2)
 tx_sig  = np.concatenate((np.zeros(int(data_gap/2)),np.linspace(0,A,N_tx-data_gap),np.zeros(int(data_gap/2))),axis=None)
 
-# A = 0.9 * 2**15  # -6 dBFS
-# B = 1e6
-# N = int(samples_per_frame)
-# T = N / fs
-# t = np.linspace(-T / 2, T / 2, N, endpoint=False)
-# tx_sig = A * np.sin(2 * math.pi * B * t)
-# 
 conv.tx([tx_sig, tx_sig])
 
 tddn.sync_soft  = 0
 tddn.sync_soft  = 1
- 
+
 rx_t = np.linspace(0,N_rx, N_rx , endpoint=False)
+fig, (ch1, ch2) = plt.subplots(1, 2)
 
-for r in range(200):
+stdout, stderr = ssh._run(f"busybox devmem 0x{tx_dma_base_addr + x_length_dma:02x} 32")
+print(f"TX DMA x_length_dma: 0x{tx_dma_base_addr + x_length_dma:02x} (Relative {x_length_dma:02x}): {stdout}")
+
+stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + x_length_dma:02x} 32")
+print(f"RX DMA x_length_dma: 0x{rx_dma_base_addr + x_length_dma:02x} (Relative {x_length_dma:02x}): {stdout}")
+
+lenth_offload = int(int(stdout, base=16) / 64)
+
+stdout, stderr = ssh._run(f"busybox devmem 0x{rx_offload_base_addr + offload_transfer_length:02x} 32")
+print(f"rx_offload offload_transfer_length: 0x{rx_offload_base_addr + offload_transfer_length:02x} (Relative {offload_transfer_length:02x}): {stdout}")
+
+stdout, stderr = ssh._run(f"busybox devmem 0x{rx_offload_base_addr + offload_transfer_length:02x} 32 {lenth_offload}")
+
+stdout, stderr = ssh._run(f"busybox devmem 0x{rx_offload_base_addr + offload_transfer_length:02x} 32")
+print(f"rx_offload offload_transfer_length: 0x{rx_offload_base_addr + offload_transfer_length:02x} (Relative {offload_transfer_length:02x}): {stdout}")
+
+for r in range(100):
     rx_sig  = conv.rx()
-    plt.plot(rx_t, np.abs(rx_sig[0]))
-    plt.ylabel("Amplitude")
-    plt.xlabel("Samples")
-    plt.title(f"Capture number: {r}")
-    plt.pause(0.3)
-    if r % 2 ==1 :
-        conv.tx_destroy_buffer()
-        tx_sig  = np.concatenate((np.zeros(int(data_gap/2)),np.linspace(A,0,N_tx-data_gap),np.zeros(int(data_gap/2))),axis=None)
-        conv.tx([tx_sig, tx_sig])
+    ch1.plot(rx_t, np.abs(rx_sig[0]))
+    ch1.set_title("Channel 1 data")
+    ch2.plot(rx_t, np.abs(rx_sig[1]))
+    ch2.set_title("Channel 2 data")
+    fig.suptitle(f"Capture number: {r}")
+    
+    # if r % 2 ==1 :
+        # conv.tx_destroy_buffer()
+        # # tx_sig  = np.concatenate((np.zeros(data_gap),np.linspace(A,0,N_tx-(2*data_gap)),np.zeros(data_gap)),axis=None)
+        # conv.tx([tx_sig, tx_sig])
 
-    if r % 2 ==0 :
-        conv.tx_destroy_buffer()
-        tx_sig  = np.concatenate((np.zeros(int(data_gap/2)),np.linspace(0,A,N_tx-data_gap),np.zeros(int(data_gap/2))),axis=None)
-        conv.tx([tx_sig, tx_sig])
+    # if r % 2 ==0 :
+        # conv.tx_destroy_buffer()
+        # # tx_sig  = np.concatenate((np.zeros(data_gap),np.linspace(0,A,N_tx-(2*data_gap)),np.zeros(data_gap)),axis=None)
+        # conv.tx([tx_sig, tx_sig])
 plt.show()
 
 tddn.enable = 0
