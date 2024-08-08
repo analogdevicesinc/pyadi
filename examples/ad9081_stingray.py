3f3d1827dd8aec67dc162323cca40e75817e403d
PopPaul2021 <Paul.Pop@analog.com>
2024-08-08 09:29:36 +0300

    checkpoint
    Signed-off-by: PopPaul2021 <Paul.Pop@analog.com>

--- examples/ad9081_stingray.py
+++ examples/ad9081_stingray.py
@@ -46,32 +46,31 @@ conv.tx_channel_nco_frequencies = [0] * 4
 conv.rx_main_nco_frequencies = [100000000] * 4 
 conv.tx_main_nco_frequencies = [100000000] * 4 
 
-conv.rx_enabled_channels = [0]
-conv.tx_enabled_channels = [0]
+conv.rx_enabled_channels = [0,1,2]
+conv.tx_enabled_channels = [0,1,2]
 conv.rx_nyquist_zone = ["odd"] * 4
 
 conv.rx_cyclic_buffer = False
 conv.tx_ddr_offload = False
 conv.rx_ddr_offload = False
-samples_per_frame_desired = 256
+samples_per_frame_desired = 24576
 
 
 stdout, stderr = ssh._run(f"busybox devmem 0x{tx_offload_base_addr + control_reg_offload:02x} 32 0x1")
 conv.tx_cyclic_buffer = False
 tx_sig = np.zeros(samples_per_frame_desired)
-conv.tx(tx_sig)
+conv.tx([tx_sig,tx_sig,tx_sig])
 conv.tx_destroy_buffer()
 conv.tx_cyclic_buffer = True
 
 stdout, stderr = ssh._run(f"busybox devmem 0x{tx_offload_base_addr + control_reg_offload:02x} 32 0x2")
 stdout, stderr = ssh._run(f"busybox devmem 0x{rx_offload_base_addr + control_reg_offload:02x} 32 0x2")
 
-tddn.enable = 0
 frame_length_ms = 1
 pulse_width_high = (samples_per_frame_desired / conv.tx_sample_rate) * 1000
 tddn.startup_delay_ms = 0
 tddn.frame_length_ms = frame_length_ms
-tddn.burst_count = 3
+tddn.burst_count = 0
 
 # TDD ENABLE
 
@@ -82,13 +81,13 @@ tddn.channel[2].enable   = 1
 
 # TX OFFFLOAD SYNC
 tddn.channel[0].on_ms = 0
-tddn.channel[0].off_ms = pulse_width_high
+tddn.channel[0].off_raw = 1
 tddn.channel[0].polarity = 0
 tddn.channel[0].enable = 1
 
 # RX OFFFLOAD SYNC
-tddn.channel[1].on_ms = 0.0015
-tddn.channel[1].off_ms = pulse_width_high +0.0015
+tddn.channel[1].on_ms = 0
+tddn.channel[1].off_raw = 1
 tddn.channel[1].polarity = 0
 tddn.channel[1].enable = 1
 
@@ -105,7 +104,11 @@ fs = int(conv.tx_sample_rate)
 samples_per_frame = math.ceil(pulse_width_high * (fs / 1000))
 
 N_tx = int(samples_per_frame)
-N_rx = N_tx
+
+N_rx = 2*N_tx
+conv.rx_buffer_size = N_rx
+length_dma_rx  =  int((N_rx*16)-1)
+length_offload_rx =  int((length_dma_rx/64))
 
 print(f"TX Sampling_rate: {conv.tx_sample_rate}")
 print(f"RX Sampling_rate: {conv.rx_sample_rate}")
@@ -121,89 +124,65 @@ print(f"TDD_frame time[ms]: {frame_length_ms}")
 
 
 A = 0.9 * 2**15  # -6 dBFS
-B = 1e6
+B = 1e4
 N = int(samples_per_frame_desired)
 T = N / fs
 t = np.linspace(0, T, N, endpoint=False)
 tx_sig = A * np.sin(2 * math.pi * B * t)
-conv.tx(tx_sig)
-
-length_dma_rx  =  int((samples_per_frame_desired*16)-1)
-length_offload_rx =  int((length_dma_rx/64))
-
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + x_length_dma:02x} 32 {length_dma_rx}")
-ssh._run(f"busybox devmem 0x{rx_offload_base_addr + offload_transfer_length:02x} 32 {length_offload_rx}")
+conv.tx([tx_sig,tx_sig,tx_sig])
 
 
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x40c:02x} 32 0x6")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x400:02x} 32 0x1")
+# stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + x_length_dma:02x} 32 {length_dma_rx}")
+# ssh._run(f"busybox devmem 0x{rx_offload_base_addr + offload_transfer_length:02x} 32 {length_offload_rx}")
 
+# stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x40c:02x} 32 0x6")
+# stdout, stderr = ssh._run(f"busybox devmem 0x{tx_dma_base_addr + 0x40c:02x} 32 0x7")
+
+
+# stdout, stderr = ssh._run(f"busybox devmem 0x{tx_dma_base_addr + x_length_dma:02x} 32 ")
+length_dma_tx     = int((samples_per_frame_desired*16)-1)
+length_offload_tx = int((length_dma_tx/64))
+# stdout, stderr = ssh._run(f"busybox devmem 0x{tx_dma_base_addr + x_length_dma:02x} 32 {length_dma_tx}")
+# ssh._run(f"busybox devmem 0x{tx_offload_base_addr + offload_transfer_length:02x} 32 {length_offload_tx}")
+
+
+print(f"computed rx_dma_base_addr + x_length_dma: {length_dma_rx:02x}")
+print(f"computed tx_dma_base_addr + x_length_dma:  {length_dma_tx:02x}")
+print(f"computed tx_offload_base_addr + offload_transfer_length: {length_offload_tx:02x}")
+print(f"computed rx_offload_base_addr + offload_transfer_length: {length_offload_rx:02x}")
+   
 stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + x_length_dma:02x} 32")
-print(f"RX DMA x_length_dma: 0x{rx_dma_base_addr + x_length_dma:02x} (Relative {rx_dma_base_addr:02x}): {stdout}")
+print(f"rx_dma_base_addr + x_length_dma: 0x{rx_dma_base_addr + x_length_dma:02x} (Relative {x_length_dma:02x}): {stdout}")
 stdout, stderr = ssh._run(f"busybox devmem 0x{tx_dma_base_addr + x_length_dma:02x} 32")
-print(f"TX DMA x_length_dma: 0x{tx_dma_base_addr + x_length_dma:02x} (Relative {tx_dma_base_addr:02x}): {stdout}")
-
-
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_offload_base_addr + offload_transfer_length:02x} 32")
-print(f"TX DMA x_length_dma: 0x{rx_offload_base_addr + offload_transfer_length:02x} (Relative {offload_transfer_length:02x}): {stdout}")
+print(f"tx_dma_base_addr + x_length_dma: 0x{tx_dma_base_addr + x_length_dma:02x} (Relative {x_length_dma:02x}): {stdout}")
 
 stdout, stderr = ssh._run(f"busybox devmem 0x{tx_offload_base_addr + offload_transfer_length:02x} 32")
-print(f"TX DMA x_length_dma: 0x{tx_offload_base_addr + offload_transfer_length:02x} (Relative {offload_transfer_length:02x}): {stdout}")
+print(f"tx_offload_base_addr + offload_transfer_length: 0x{tx_offload_base_addr + offload_transfer_length:02x} (Relative {offload_transfer_length:02x}): {stdout}")
+stdout, stderr = ssh._run(f"busybox devmem 0x{rx_offload_base_addr + offload_transfer_length:02x} 32")
+print(f"rx_offload_base_addr + offload_transfer_length: 0x{rx_offload_base_addr + offload_transfer_length:02x} (Relative {offload_transfer_length:02x}): {stdout}")
 
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x410:02x} 32")
-print(f"TX DMA x_length_dma: 0x{rx_dma_base_addr + 0x410:02x} (Relative {0x410:02x}): {stdout}")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x400:02x} 32")
-print(f"TX DMA x_length_dma: 0x{rx_dma_base_addr + 0x400:02x} (Relative {0x400:02x}): {stdout}")
-stdout, stderr = ssh._run(f"busybox devmem 0x{tx_dma_base_addr + 0x40c:02x} 32")
-print(f"TX DMA x_length_dma: 0x{tx_dma_base_addr + 0x40c:02x} (Relative {0x40c:02x}): {stdout}")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x40c:02x} 32")
-print(f"rX DMA x_length_dma: 0x{rx_dma_base_addr + 0x40c:02x} (Relative {0x40c:02x}): {stdout}")
 
-rx_buf1_base_adress = 42213376
-rx_buf2_base_adress = rx_buf1_base_adress + length_dma_rx + 1
-rx_buf3_base_adress = rx_buf2_base_adress + length_dma_rx + 1
-
-print(f"{rx_buf1_base_adress:02x}")
-print(f"{rx_buf2_base_adress:02x}")
-print(f"{rx_buf3_base_adress:02x}")
-
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x410:02x} 32 {rx_buf1_base_adress}")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x408:02x} 32 0x1")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x410:02x} 32 {rx_buf2_base_adress}")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x408:02x} 32 0x1")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x410:02x} 32 {rx_buf3_base_adress}")
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x408:02x} 32 0x1")
-
-# to test with 4 submits 
 
 tddn.sync_soft  = 0
 tddn.sync_soft  = 1
 
-time.sleep(3)
-
-stdout, stderr = ssh._run(f"busybox devmem 0x{rx_dma_base_addr + 0x428:02x} 32")
-print(f"RX DMA TRANSFER_DONE: 0x{rx_dma_base_addr + 0x428:02x} (Relative {0x428:02x}): {stdout}")
-
-
-cnt=0
-rx_buf_1 = [0 for x in range(N_rx)]
-rx_buf_2 = [0 for x in range(N_rx)]
-rx_buf_3 = [0 for x in range(N_rx)]
-
-for i in range(0,length_dma_rx+1,16):
-    stdout, stderr = ssh._run(f"busybox devmem 0x{rx_buf1_base_adress + i:02x} 16")
-    rx_buf_1[cnt] = twos_complement(stdout, 16) 
-    stdout, stderr = ssh._run(f"busybox devmem 0x{rx_buf2_base_adress + i:02x} 16")
-    rx_buf_2[cnt] = twos_complement(stdout, 16) 
-    stdout, stderr = ssh._run(f"busybox devmem 0x{rx_buf3_base_adress + i:02x} 16")
-    rx_buf_3[cnt] = twos_complement(stdout, 16) 
-    cnt+=1
 
 rx_t = np.linspace(0,N_rx, N_rx , endpoint=False)
+capture_range = 200
+enabled_channels = 3
+rx_sig = np.zeros((capture_range,enabled_channels,N_rx)) 
 
-plt.plot(rx_t,rx_buf_1)
-plt.plot(rx_t,rx_buf_2)
-plt.plot(rx_t,rx_buf_3)
+for r in range(capture_range):
+    rx_sig[r]  = conv.rx()
+
+fig, (ch1, ch2) = plt.subplots(1, 2)
+
+for r in range(capture_range):
+    plt.suptitle(f"Capture number: {r}")
+    ch1.plot(rx_t, np.abs(rx_sig[r][0]))
+    ch1.set_title("Channel 1 data")
+    ch2.plot(rx_t, np.abs(rx_sig[r][1]))
+    ch2.set_title("Channel 2 data")
 plt.show()
 
 tddn.enable = 0
