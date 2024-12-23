analogdevicesinc/pyadi-iio

Add support for AD7091R-2, AD7091R-4 and AD7091R-8
Add ad7091r.py driver
Add test script, example code
Update index.rst, init.py, supported_parts.md files
Add xml emu context file
Update hardware_map.yml file for test emulation

Signed-off-by: SGudla <Saikiran.Gudla@analog.com>

adi/__init__.py
@@ -23,6 +23,7 @@ from adi.ad5686 import ad5686
 from adi.ad5754r import ad5754r
 from adi.ad5940 import ad5940
 from adi.ad6676 import ad6676
+from adi.ad7091r import ad7091rx
 from adi.ad7124 import ad7124
 from adi.ad7134 import ad7134
 from adi.ad7291 import ad7291

analogdevicesinc/pyadi

adi/__init__.py
@@ -23,6 +23,7 @@ from adi.ad5686 import ad5686
 from adi.ad5754r import ad5754r
 from adi.ad5940 import ad5940
 from adi.ad6676 import ad6676
+from adi.ad7091rx import ad7091rx
 from adi.ad7124 import ad7124
 from adi.ad7134 import ad7134
 from adi.ad7291 import ad7291